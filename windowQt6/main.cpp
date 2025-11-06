f7ee8fa9d43642ad36c1dd6ffe5938297542d6a0
chenxing zhang <chenxing88.zhang@gmail.com>
2025-11-06 21:50:49 +0100

    Add files via upload

--- windowQt6/main.cpp
+++ windowQt6/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
-
+#include "luahelper.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    LuaHelper *lua = new LuaHelper ;
+
     MainWindow w;
     w.show();
     return a.exec();
