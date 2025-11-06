06f0fa1e785cae181828a1b492aa790f4e2e7e86
chenxing <chenxing88.zhang@gmail.com>
2025-11-06 22:22:19 +0100

    first version

---
+++ build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_windowQt6_FILE C:/work_zcx/QtProject/windowQt6/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/windowQt6.exe)
+set(__QT_DEPLOY_TARGET_windowQt6_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_windowQt6_RUNTIME_DLLS C:/Qt/Qt6/6.10.0/mingw_64/bin/Qt6Widgets.dll;C:/Poco/install/bin/PocoNetd.dll;C:/Poco/install/bin/PocoUtild.dll;C:/Poco/install/bin/PocoJSONd.dll;C:/Poco/install/bin/PocoXMLd.dll;C:/Qt/Qt6/6.10.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/Qt6/6.10.0/mingw_64/bin/Qt6Core.dll;C:/Poco/install/bin/PocoFoundationd.dll)
